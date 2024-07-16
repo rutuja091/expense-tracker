cb50d4245faa67e7dea1c94f5e73bf111ad557da
rutuja091 <gaikwadrutuja254@gmail.com>
2024-07-16 17:11:30 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+//connect to MOngoDB
+
+const connectDB=async()=>{
+    const conn=await mongoose.connect(process.env.MONGODB_URL)
+
+    if (conn){
+        console.log(`MOngoDB connected Successfully ✅`);
+    }
+};
+
+connectDB();
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:`welcome to expense API`
+    })
+})
+
+
+const PORT = process.env.PORT ||5000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+})
+
